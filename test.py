46a48cfb473822f0f2cc140728fa6a97f728f310
ymt117 <yamamto117@gmail.com>
2020-07-31 10:05:46 +0900

    テストケースを追加

--- test.py
+++ test.py
@@ -7,5 +7,9 @@ class TestPoint(unittest.TestCase):
         self.assertEqual(0.0, p.get_x())
         self.assertEqual(0.0, p.get_y())
 
+        p = Point(1.00, 1.00)
+        self.assertEqual(1.0, p.get_x())
+        self.assertEqual(1.0, p.get_y())
+
 if __name__ == "__main__":
     unittest.main()
